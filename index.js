fcfbe8f24ab49b756a462011d55484e48124ab2c
Erick Tabuchi Matsunaga <erick-matsunaga@dgt-consultoria.com.br>
2020-04-28 11:13:50 -0300

    Teste enviando arquivo direto

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(bodyParser.json())
 
 app.listen(process.env.PORT || 8000);
 
-app.post('/wordTEST', async (req, res) => {
+// POST SEM BUFFER DE BASE 64 => Retorna apenas Base64
+app.post('/wordBase64', async (req, res) => {
 
   const doc = new Document();
 
@@ -36,7 +37,6 @@ app.post('/wordTEST', async (req, res) => {
           
           
   const b64string = await Packer.toBase64String(doc)
-  console.log(b64string)
   res.end(b64string)
 
 
@@ -50,7 +50,46 @@ app.post('/wordTEST', async (req, res) => {
 
 });
 
-app.get("/word", async (req, res) => {
+// POST COM BUFFER DE BASE 64 => Retorno Arquivo Pronto
+app.post('/wordBuffer', async (req, res) => {
+
+  const doc = new Document();
+
+  doc.addSection({
+    properties: {},
+    children: [
+        new Paragraph({
+            children: [
+                new TextRun(req.body.testeManeiro),
+                new TextRun("Hello World"),
+                new TextRun({
+                    text: "Foo Barrrrrrrr",
+                    bold: true,
+                }),
+                new TextRun({
+                    text: "\tGithub is the best",
+                    bold: true,
+                }),
+            ],
+        }),
+    ],
+  });
+          
+          
+  const b64string = await Packer.toBase64String(doc)
+
+  res.setHeader('Content-Disposition', 'attachment; filename=My Document.docx');
+  var doc64 = Buffer.from(b64string, "base64");
+    res.writeHead(200, {
+    'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    'Content-Length': doc64.length
+  });
+  res.end(doc64);
+
+});
+
+// Teste GET levando URL dowload do arquivo
+app.get("/wordGET", async (req, res) => {
 
   console.log("executing....");
   const doc = new Document();
