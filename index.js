73aa696f01662d90bad32781aeddd3537be1de5a
Erick Tabuchi Matsunaga <erick-matsunaga@dgt-consultoria.com.br>
2020-04-28 11:07:52 -0300

    Mudanças no b64...

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ const express = require('express')
 const app = express();
 const docx = require('docx');
 const dotenv = require('dotenv');
-
 const bodyParser = require('body-parser');
+const { Document, Packer, Paragraph, Table, TableCell, TableRow, TextRun  } = docx;
 
-const { Document, Packer, Paragraph, Table, TableCell, TableRow,TextRun  } = docx;
 dotenv.config()
-
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
 app.listen(process.env.PORT || 8000);
 
 app.post('/wordTEST', async (req, res) => {
@@ -24,21 +19,34 @@ app.post('/wordTEST', async (req, res) => {
     children: [
         new Paragraph({
             children: [
-                new TextRun(req.body.testeManeiro)
+                new TextRun(req.body.testeManeiro),
+                new TextRun("Hello World"),
+                new TextRun({
+                    text: "Foo Barrrrrrrr",
+                    bold: true,
+                }),
+                new TextRun({
+                    text: "\tGithub is the best",
+                    bold: true,
+                }),
             ],
         }),
     ],
-});
-
-  const b64string = await Packer.toBase64String(doc)
-
-  res.setHeader('Content-Disposition', 'attachment; filename=My Document.docx');
-  var doc64 = Buffer.from(b64string, "base64");
-    res.writeHead(200, {
-    'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-    'Content-Length': doc64.length
   });
-  res.end(doc64);
+          
+          
+  const b64string = await Packer.toBase64String(doc)
+  console.log(b64string)
+  res.end(b64string)
+
+
+  // res.setHeader('Content-Disposition', 'attachment; filename=My Document.docx');
+  // var doc64 = Buffer.from(b64string, "base64");
+  //   res.writeHead(200, {
+  //   'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  //   'Content-Length': doc64.length
+  // });
+  // res.end(doc64);
 
 });
 
@@ -64,7 +72,7 @@ app.get("/word", async (req, res) => {
             ],
         }),
     ],
-});
+  });
 
   const b64string = await Packer.toBase64String(doc)
   console.log(b64string)
