96adb08d8a052c46a9900321f0f6fe253830f179
Erick Tabuchi Matsunaga <erick-matsunaga@dgt-consultoria.com.br>
2020-04-30 12:27:31 -0300

    Teste 8

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.post('/wordBase64', async (req, res) => {
   doc.addSection({
     properties: {},
     children: [
-        req.body.testeManeiro.forEach(element => {
+        req.body.testeManeiro.map(element => {
           new Paragraph({
               children: [
                   new TextRun(element)
